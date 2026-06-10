5bb7b811c7e684db80a169633d1838888fad06c7
Chamindu18 <rachranasinghe@students.nsbm.ac.lk>
2026-06-10 21:42:44 +0530

    feat: setup express backend with typescript

---
+++ server/src/app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("NSBM LostLink API Running");
+});
+
+export default app;
