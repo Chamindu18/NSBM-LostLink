476f72e08ddcec163da79de8ab5d2d4fbc6c4e7e
Chamindu18 <rachranasinghe@students.nsbm.ac.lk>
2026-06-11 20:02:35 +0530

    chore: setup authentication module structure

--- server/src/validators/auth.validator.ts
+++ server/src/validators/auth.validator.ts
@@ -1,8 +1,13 @@
 import { z } from "zod";
 
 export const registerSchema = z.object({
-  studentId: z.string(),
-  name: z.string(),
+  studentId: z.string().min(1),
+  name: z.string().min(2),
   email: z.email(),
-  password: z.string().min(6)
+  password: z.string().min(6),
+});
+
+export const loginSchema = z.object({
+  email: z.email(),
+  password: z.string(),
 });
